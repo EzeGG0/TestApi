054df132ab780dede633b2eddffc708801b5447d
Eze <94138154+EzeGG0@users.noreply.github.com>
2022-10-10 19:33:21 -0300

    navBar js

--- js/actions.js
+++ js/actions.js
@@ -1,8 +1,4 @@
 $(document).ready(() => {
   //! Working
-  // Get width
-  $(window).resize(() => {
-    const size = $(window).width();
-    console.log(size);
-  });
+
 });
--- js/navBar.js
+++ js/navBar.js
@@ -1,1 +1,20 @@
 //! Not started
+$(document).ready(() => {
+  // Get width
+  const sizeLoad = $(window).width();
+  if (sizeLoad <= 790) {
+    $('#navBarId').removeClass('navBar');
+    $('#navBarId').addClass('navBarHidden');
+  }
+  $(window).resize(() => {
+    const size = $(window).width();
+    if (size <= 790) {
+      $('#navBarId').removeClass('navBar');
+      $('#navBarId').addClass('navBarHidden');
+    } else if (!$('#navBarId').hasClass('navBar')) {
+      $('#navBarId').addClass('navBar');
+    } else if ($('#navBarId').hasClass('navBarHidden')) {
+      $('#navBarId').removeClass('navBarHidden');
+    }
+  });
+});
