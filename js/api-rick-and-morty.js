975d7b3e393daa7b8dbf5d07310ce374d702ed57
Eze <94138154+EzeGG0@users.noreply.github.com>
2022-10-09 21:36:37 -0300

    problema solucionado

--- js/api-rick-and-morty.js
+++ js/api-rick-and-morty.js
@@ -5,24 +5,20 @@ $(document).ready(() => {
   const container = document.querySelector('#container');
   const quantity = document.querySelector('#submitQuantity');
   const quantityInput = document.querySelector('#characterQuantity');
-  let cantidadPersonajes = 0;
+  let cantidadPersonajes = 30;
   // Seleccionar el personaje
-  quantity.addEventListener('click', (e) => {
-    e.preventDefault();
-    const quantitySelected = quantityInput.value;
-    const quantityEnd = parseInt(quantitySelected);
-    cantidadPersonajes = quantityEnd;
-    // funcion para mostrar la informacion de los personajes en el html
 
-    const showCharacter = (character) => {
-      // Se descompone todo las variables para que se mas facil llamarlas sino es: character.name etc, asi es solo name
-      const {
-        id, name, gender, location, origin, species, status, image,
-      } = character;
-      // Se crea un contenedor de las cards
-      const createDiv = document.createElement('div');
-      // Se hace que se muestre toda la informacion de la api en el html
-      const innerText = `
+  // funcion para mostrar la informacion de los personajes en el html
+
+  const showCharacter = (character) => {
+  // Se descompone todo las variables para que se mas facil llamarlas sino es: character.name etc, asi es solo name
+    const {
+      id, name, gender, location, origin, species, status, image,
+    } = character;
+    // Se crea un contenedor de las cards
+    const createDiv = document.createElement('div');
+    // Se hace que se muestre toda la informacion de la api en el html
+    const innerText = `
   <div class="charactersCard">
     <img src="${image}" alt="${name} Image" class="characterImg"/>
     <h2 class="characterName" id="charName">${name}</h2>
@@ -34,43 +30,48 @@ $(document).ready(() => {
     <p class="location">Location: <br>${location.name}</p>
   </div>
   `;
-      // Se agrega al DOM
-      createDiv.innerHTML = innerText;
-      container.appendChild(createDiv);
-    };
+    // Se agrega al DOM
+    createDiv.innerHTML = innerText;
+    container.appendChild(createDiv);
+  };
 
-    const getCharacter = async (id) => {
-      try {
-        // Se conecta a la api
-        const response = await fetch(`${baseUrl}/character/${id}`);
-        // Se soluciona el problema de cors
-        const responseJson = await response.json();
-        //   console.log(responseJson);
-        showCharacter(responseJson);
-      } catch (error) {
-        console.log(`El error es: ${error}`);
-      }
-    };
-    // Se crear el bucle para iterar sobre el id de los personajes
-    const rickFetcher = async () => {
-      // Bucle para iterar sobre cada id
-      for (let i = 1; i <= cantidadPersonajes; i++) {
-        await getCharacter(i); // Se espera la respuesta de la api
-      }
-    };
-    rickFetcher();
+  const getCharacter = async (id) => {
+    try {
+    // Se conecta a la api
+      const response = await fetch(`${baseUrl}/character/${id}`);
+      // Se soluciona el problema de cors
+      const responseJson = await response.json();
+      //   console.log(responseJson);
+      showCharacter(responseJson);
+    } catch (error) {
+      console.log(`El error es: ${error}`);
+    }
+  };
+  // Se crear el bucle para iterar sobre el id de los personajes
+  const rickFetcher = async (cantidad) => {
+  // Bucle para iterar sobre cada id
+    for (let i = 1; i <= cantidad; i++) {
+      await getCharacter(i); // Se espera la respuesta de la api
+    }
+  };
 
-    const submit = document.querySelector('#submitCharacter');
-    const nameChar = document.querySelector('#characterName');
-    //   console.log(nameChar);
-    // Agregar un personaje mediante un input
-    submit.addEventListener('click', async (ev) => {
-      ev.preventDefault();
-      const character = nameChar.value;
-      const characterId = parseInt(character);
-      await getCharacter(characterId);
-    });
-
-  // Rick and Morty api
+  const submit = document.querySelector('#submitCharacter');
+  const nameChar = document.querySelector('#characterName');
+  //   console.log(nameChar);
+  // Agregar un personaje mediante un input
+  submit.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const character = nameChar.value;
+    const characterId = parseInt(character);
+    await getCharacter(characterId);
   });
+  // Seleccionar cantidad de personajes
+  quantity.addEventListener('click', (e) => {
+    e.preventDefault();
+    const quantitySelected = quantityInput.value;
+    const quantityEnd = parseInt(quantitySelected);
+    cantidadPersonajes = quantityEnd;
+    rickFetcher(cantidadPersonajes);
+  });
+  // Rick and Morty api
 });
