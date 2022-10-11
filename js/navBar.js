ef82b308a298f7ce4a2020d43a3d8eff6d7546e3
Eze <94138154+EzeGG0@users.noreply.github.com>
2022-10-11 02:44:10 -0300

    algo del meno (mejorando)

--- js/navBar.js
+++ js/navBar.js
@@ -5,16 +5,32 @@ $(document).ready(() => {
   if (sizeLoad <= 790) {
     $('#navBarId').removeClass('navBar');
     $('#navBarId').addClass('navBarHidden');
+    $('#hambNavMenu').removeClass('hidden');
+    //   $('#navBarId').addClass('mobileNavBar');
+    $('#hambNavMenu').click((e) => {
+      console.log(e);
+      $('#navBarId').toggleClass('mobileNavBar');
+      $('#hambNavMenu').toggleClass('hambMenuShowing');
+    });
   }
   $(window).resize(() => {
     const size = $(window).width();
     if (size <= 790) {
       $('#navBarId').removeClass('navBar');
       $('#navBarId').addClass('navBarHidden');
+      $('#hambNavMenu').removeClass('hidden');
+      //   $('#navBarId').addClass('mobileNavBar');
+      $('#hambNavMenu').click((e) => {
+        console.log(e);
+        $('#navBarId').toggleClass('mobileNavBar');
+        $('#hambNavMenu').toggleClass('hambMenuShowing');
+      });
     } else if (!$('#navBarId').hasClass('navBar')) {
       $('#navBarId').addClass('navBar');
-    } else if ($('#navBarId').hasClass('navBarHidden')) {
       $('#navBarId').removeClass('navBarHidden');
+      $('#hambNavMenu').addClass('hidden');
+      $('#hambNavMenu').addClass('transition');
+      $('#navBarId').removeClass('mobileNavBar');
     }
   });
 });
